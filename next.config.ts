9037e6687452ca9bfc7137d06adfc02e1a9e262b
kshithijm1 <kshithij.m@gmail.com>
2026-05-18 04:18:10 -0400

    github actions

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // <-- ADD THIS LINE
+  images: {
+    unoptimized: true, // <-- ADD THIS LINE (GitHub Pages doesn't support Next.js default image optimization)
+  },
 };
 
 export default nextConfig;
